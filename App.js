44c9d2880fb79847ada3cdc5e1805a4fa53f22c7
Rishabh Chatterjee <rchatterjee@podfather.com>
2023-08-19 15:35:16 +0100

    Use faMugSaucer in App.js (does not compile for web)

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faMugSaucer } from '@fortawesome/free-solid-svg-icons/faMugSaucer'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> 
+        Open up App.js to start working on your app!
+        <FontAwesomeIcon icon={faMugSaucer}/>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
